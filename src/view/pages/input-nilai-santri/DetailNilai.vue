6095b8e3e8e4e55f7eff240d8ced60c4e6ac959d
Yosi Mardianti <yorermrs@gmail.com>
2023-09-07 16:51:11 +0700

    update exp, detail cttn

--- src/view/pages/input-nilai-santri/DetailNilai.vue
+++ src/view/pages/input-nilai-santri/DetailNilai.vue
@@ -89,6 +89,16 @@
       </b-collapse>
     </div> -->
 
+    <div align="center" v-if="printingInProgress">
+      <v-progress-circular
+        color="primary"
+        indeterminate
+        :siize="40"
+        :wdth="5"
+      ></v-progress-circular>
+      <p>{{ progressData }}%</p>
+    </div>
+
     <div class="card cardHover">
       <v-data-table 
         :headers="headers" 
@@ -117,6 +127,26 @@
           hide-details
           ></v-text-field>
           <v-spacer></v-spacer>
+          <div>
+              <b-dropdown
+                block
+                variant="primary"
+                class="mr-4 font-weight-bold rounded-xl"
+                text="CETAK">
+                <b-dropdown-item @click="openModalExport">PDF</b-dropdown-item>
+                <b-dropdown-item>
+                  <vue-excel-xlsx
+                    :data="dataDetail"
+                    :columns="columnsExcel"
+                    :file-name="'data-nilai-santri'"
+                    :file-type="'xlsx'"
+                    :sheet-name="'Data'"
+                    >
+                    EXCEL
+                  </vue-excel-xlsx>
+                </b-dropdown-item>
+              </b-dropdown>
+            </div>
           <v-dialog
             v-model="dialog"
             max-width="1200px"
@@ -128,7 +158,6 @@
                 dark
                 v-bind="attrs"
                 v-on="on"
-                rounded
                 v-show="accessList.C"
                 >
                 <i class="flaticon-add-circular-button mr-1 text-white"></i>
@@ -228,7 +257,6 @@
                                   <v-text-field
                                       v-model="data.praktek"
                                       label="Praktek"
-                                      type="number"
                                       clearable
                                       name="praktek"
                                       color="#ee8b3d"
@@ -270,8 +298,55 @@
               </v-card>
             </form>
           </v-dialog>
+          <v-dialog v-model="dialogSeenBill" max-width="800px">
+            <v-card>
+              <v-card-title class="border">
+                  <span class="text-h5">Detail Catatan</span>
+                  <v-spacer></v-spacer>
+                  <v-icon
+                      class="rounded-circle p-2 shadow-sm"
+                      small
+                      @click="dialogSeenBill = false"
+                      color="#000"
+                  >
+                      mdi-close
+                  </v-icon>
+              </v-card-title>
+
+              <v-card-text>
+                  <v-container>
+                    <h6>{{ dataDetailCttn }}</h6>
+                  </v-container>
+              </v-card-text>
+              <v-divider></v-divider>
+              <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn class="text-xl-subtitle-1" color="#73a4ef" text @click="dialogSeenBill = false">
+                  Tutup
+                  </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
           </v-toolbar>
         </template>
+
+        <template v-slot:[`item.keterangan_nilai`]="{ item }">
+          <v-btn
+              color="#73a4ef"
+              light
+              small
+              fab
+              class="ml-3"
+              @click="seenFileBill(item)"
+              data-toggle="tooltip"
+              title="Lihat Catatan"
+              style="text-transform: capitalize !important; min-width: 0px; padding: 0 6px;"
+          >
+          <v-icon small color="white">
+              mdi-message
+          </v-icon>
+          </v-btn>
+      </template>
   
         <!-- <template v-slot:item.actions="{ item }">
           <v-tooltip top>
@@ -295,8 +370,273 @@
         </template> -->
       </v-data-table>
     </div>
+
+    <v-dialog
+      v-model="dialogCetak"
+      max-width="600px"
+      persistent
+    >
+      <v-card>
+        <v-card-title class="border">
+          <span class="text-h5">Cetak Data</span>
+          <v-spacer></v-spacer>
+          <v-icon
+            class="rounded-circle p-2 shadow-sm"
+            small
+            @click="closeModalExport"
+            color="#000"
+          >
+            mdi-close
+          </v-icon>
+        </v-card-title>
+
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col
+                cols="12"
+                md="12"
+              >
+                <v-textarea
+                  v-model="formExport.judul"
+                  label="Judul"
+                  required
+                  clearable
+                  color="#ee8b3d"
+                  rows="1"
+                ></v-textarea>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col
+                cols="12"
+                md="12"
+              >
+                <v-textarea
+                  v-model="formExport.sub_judul"
+                  label="Sub Judul"
+                  required
+                  clearable
+                  color="#ee8b3d"
+                  rows="2"
+                ></v-textarea>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <button
+            @click="generateReportAll()"
+            class="btn btn-primary btn-sm font-weight-bolder text-md-body-1 rounded-lg py-2 mb-3 mr-3 w-100px"
+          >
+            Cetak
+          </button>
+          <button
+            type="button"
+            @click="closeModalExport"
+            class="btn btn-light-primary btn-sm font-weight-bolder text-md-body-1 rounded-lg py-2 mb-3 w-100px"
+          >
+            Batal
+          </button>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <!-- PDF ALL Session -->
+    <template>
+      <div>
+        <vue-html2pdf
+          :show-layout="false"
+          :float-layout="true"
+          :enable-download="false"
+          :preview-modal="true"
+          :paginate-elements-by-height="1400"
+          :pdf-quality="2"
+          :manual-pagination="false"
+          :html-to-pdf-options="htmlToPdfAllOptions"
+          pdf-content-width="100%"
+          @progress="onProgress($event)"
+          @hasStartedGeneration="hasStartedGeneration()"
+          @hasGenerated="hasGenerated($event)"
+          @beforeDownload="beforeDownload($event)"
+          @hasDownloaded="hasDownloaded($event)"
+          ref="html2PdfAll"
+        >
+          <section slot="pdf-content">
+            <section class="pdf-item">
+              <div
+                align="center"
+                style="
+                  font-family: 'Verdana';
+                  font-style: normal;
+                  font-size: 11pt;
+                  color: black;
+                  "
+                >
+                {{ formExport.judul }}
+                <br>{{ formExport.sub_judul }}
+              </div>
+              <br>
+              <div align="left">
+                <table border="1" cellspacing="0" cellpadding="4" width="100%" style="text-color:black;overflow:wrap;border-collapse: collapse;">
+                  <thead>
+                    <tr align="center">
+                      <th
+                        scope="col"
+                        style="font-family: 'Verdana'; font-style: normal; font-size: 12px;"
+                      >
+                      No.
+                      </th>
+                      <th
+                        scope="col"
+                        style="font-family: 'Verdana'; font-style: normal; font-size: 12px;"
+                      >
+                      Kode Santri
+                      </th>
+                      <th
+                        scope="col"
+                        style="font-family: 'Verdana'; font-style: normal; font-size: 12px;"
+                      >
+                      Nama
+                      </th>
+                      <th
+                        scope="col"
+                        style="font-family: 'Verdana'; font-style: normal; font-size: 12px;"
+                      >
+                      Tugas
+                      </th>
+                      <th
+                        scope="col"
+                        style="font-family: 'Verdana'; font-style: normal; font-size: 12px;"
+                      >
+                      UTS
+                      </th>
+                      <th
+                        scope="col"
+                        style="font-family: 'Verdana'; font-style: normal; font-size: 12px;"
+                      >
+                      UAS
+                      </th>
+                      <th
+                        scope="col"
+                        style="font-family: 'Verdana'; font-style: normal; font-size: 12px;"
+                      >
+                      Praktek
+                      </th>
+                      <th
+                        scope="col"
+                        style="font-family: 'Verdana'; font-style: normal; font-size: 12px;"
+                      >
+                      Kehadiran
+                      </th>
+                      <th
+                        scope="col"
+                        style="font-family: 'Verdana'; font-style: normal; font-size: 12px;"
+                      >
+                      Total Pertemuan
+                      </th>
+                      <th
+                        scope="col"
+                        style="font-family: 'Verdana'; font-style: normal; font-size: 12px;"
+                      >
+                      Hasil Akhir
+                      </th>
+                      <th
+                        scope="col"
+                        style="font-family: 'Verdana'; font-style: normal; font-size: 12px;"
+                      >
+                      KKM
+                      </th>
+                      <th
+                        scope="col"
+                        style="font-family: 'Verdana'; font-style: normal; font-size: 12px;"
+                      >
+                      Status
+                      </th>
+                      <th
+                        scope="col"
+                        style="font-family: 'Verdana'; font-style: normal; font-size: 12px;"
+                      >
+                      Predikat
+                      </th>
+                      <th
+                        width="10%"
+                        scope="col"
+                        style="font-family: 'Verdana'; font-style: normal; font-size: 12px;"
+                      >
+                      Catatan
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="(data, index) in dataDetail">
+                      <td align="center" style="font-family: Verdana; font-style: normal; font-size: 12px;">
+                        {{ index + 1 }}
+                      </td>
+                      <td align="left" style="font-family: Verdana; font-style: normal; font-size: 12px;">
+                        {{ data.kode_santri }}
+                      </td>
+                      <td align="left" style="font-family: Verdana; font-style: normal; font-size: 12px;">
+                        {{ data.nama_lengkap_santri }}
+                      </td>
+                      <td align="center" style="font-family: Verdana; font-style: normal; font-size: 12px;">
+                        {{ data.tugas }}
+                      </td>
+                      <td align="center" style="font-family: Verdana; font-style: normal; font-size: 12px;">
+                        {{ data.uts }}
+                      </td>
+                      <td align="center" style="font-family: Verdana; font-style: normal; font-size: 12px;">
+                        {{ data.uas }}
+                      </td>   
+                      <td align="center" style="font-family: Verdana; font-style: normal; font-size: 12px;">
+                        {{ data.praktek }}
+                      </td> 
+                      <td align="center" style="font-family: Verdana; font-style: normal; font-size: 12px;">
+                        {{ data.kehadiran }}
+                      </td>
+                      <td align="center" style="font-family: Verdana; font-style: normal; font-size: 12px;">
+                        {{ data.total_pertemuan }}
+                      </td>
+                      <td align="center" style="font-family: Verdana; font-style: normal; font-size: 12px;">
+                        {{ data.hasil_akhir }}
+                      </td>   
+                      <td align="center" style="font-family: Verdana; font-style: normal; font-size: 12px;">
+                        {{ data.kkm }}
+                      </td>
+                      <td align="center" style="font-family: Verdana; font-style: normal; font-size: 12px;">
+                        {{ data.status }}
+                      </td>
+                      <td align="center" style="font-family: Verdana; font-style: normal; font-size: 12px;">
+                        {{ data.predikat }}
+                      </td>               
+                      <td align="left" style="font-family: Verdana; font-style: normal; font-size: 12px;">
+                        {{ data.keterangan_nilai }}
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </section>
+          </section>
+        </vue-html2pdf>
+      </div>
+    </template>
+    <!-- END PDF ALL Session -->
   </div>
 </template>
+
+<style>
+td, th {
+    padding: 4px;
+    color: #000;
+    vertical-align: top;
+}
+tr { 
+    page-break-inside: avoid !important; 
+}
+</style>
     
 <script>
 import Services from "@/core/services/aljazary-api/Services";
@@ -304,6 +644,7 @@ import ApiService from "@/core/services/api.service";
 import localStorage from "@/core/services/store/localStorage";
 import { formatDate } from "@/helpers/helper.js";
 import Swal from 'sweetalert2'
+import VueHtml2pdf from "vue-html2pdf";
 
 export default {
   name: 'detail_nilai_santri',
@@ -322,6 +663,10 @@ export default {
       required: false,
       default: () => []
     }
+  },
+
+  components: {
+    VueHtml2pdf
   },
 
   created() {
@@ -446,7 +791,80 @@ export default {
     SantriCttn: [],
     showTable: false,
     idTahun: "",
-    idMapel: ""
+    idMapel: "",
+    dataDetailCttn: "",
+    dialogSeenBill: false,
+    dialogCetak: false,
+    formExport: {
+      judul: "",
+      sub_judul: ""
+    },
+    htmlToPdfAllOptions: {
+      margin: 0.4,
+      filename: `Data Nilai Santri.pdf`,
+      jsPDF: {
+        unit: "in",
+        format: "a4",
+        orientation: "landscape"
+      }
+    },
+    printingInProgress: false,
+    progressData: 0,
+    columnsExcel : [
+      {
+        label: "Kode Santri",
+        field: "kode_santri",
+      },
+      {
+        label: "Nama",
+        field: "nama_lengkap_santri"
+      },
+      {
+        label: "Tugas",
+        field: "tuga"
+      },
+      {
+        label: "UTS",
+        field: "uts"
+      },
+      {
+        label: "UAS",
+        field: "uas"
+      },
+      {
+        label: "Praktek",
+        field: "praktek"
+      },
+      {
+        label: "Kehadiran",
+        field: "kehadiran"
+      },
+      {
+        label: "Total Pertemuan",
+        field: "total_pertemuan"
+      },
+      {
+        label: "Hasil Akhir",
+        field: "hasil_akhir"
+      },
+      {
+        label: "KKM",
+        field: "kkm"
+      },
+      {
+        label: "Status",
+        field: "status"
+      },
+      {
+        label: "Predikat",
+        field: "predikat"
+      },
+      {
+        label: "Catatan",
+        field: "keterangan_nilai"
+      }
+    ],
+    dataHeader: []
   }),
 
   computed: {
@@ -791,8 +1209,42 @@ export default {
     //   this.formFilter.tipe_nilai = ""
     //   // this.showTable = false
     // },
+
+    openModalExport(){
+      this.dialogCetak = true;
+      this.formExport.judul = "Nilai Mata Pelajaran " + this.dataHeader.mapel_nama + " Kelas " + this.dataHeader.nama_kelas
+      this.formExport.sub_judul = "Tahun Ajaran " + this.dataHeader.tahun_ajaran_nama + " Semester " + this.dataHeader.tipe_ajaran_nama
+    },
+
+    closeModalExport(){
+      this.dialogCetak = false
+    },
+
+    generateReportAll() {
+      this.dialogCetak = false;
+      this.$refs.html2PdfAll.generatePdf();
+    },
+
+    onProgress(data) {
+      this.progressData = data;
+    },
+
+    beforeDownload(){
+      this.printingInProgress = true;
+    },
+
+    hasDownloaded(){
+      this.printingInProgress = false;
+      this.progressData = 0;
+    },
+
+    seenFileBill(item) {
+      this.dataDetailCttn = item.keterangan_nilai
+      this.dialogSeenBill = true;
+    },    
     
     async getDetailCache(item) {
+      this.dataHeader = item
       this.idTahun = item.tahun_id
       this.idMapel = item.mapel_id
       
